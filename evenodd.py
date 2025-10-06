508366571f83f4068ca100aaf380a5e3b8f28a42
Srishti1806 <srishtinaikar620@gmail.com>
2025-10-06 11:18:16 +0530

    save

--- evenodd.py
+++ evenodd.py
@@ -1,24 +1,16 @@
-ef is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def count_numbers(numbers):
+def count_even_odd(numbers):
     even_count = 0
     odd_count = 0
-    prime_count = 0
 
     for n in numbers:
         if n % 2 == 0:
             even_count += 1
         else:
             odd_count += 1
+
     print("Even numbers:", even_count)
     print("Odd numbers:", odd_count)
-# Prime Number Counter Program
+
 
 def is_prime(num):
     if num <= 1:
